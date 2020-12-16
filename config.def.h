a2aaecef45802e8e64ecd6ee736a0365d9bc39b1
Jodscho <Jonathan2008@gmx.de>
2020-12-16 20:37:35 +0100

    styling

--- config.def.h
+++ config.def.h
@@ -4,13 +4,14 @@
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Droid Sans Mono:size=14"
+	"DejaVuSansMono-Bold:size=14"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#000000", "#b5bd68" },
+	[SchemeSel] = { "#000000", "#e18d30" },
+	//[SchemeSel] = { "#000000", "#b5bd68" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
