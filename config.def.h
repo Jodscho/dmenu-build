9878180e38d7b315ad37dd2aa623e35b8640993e
Jodscho <Jonathan2008@gmx.de>
2021-06-14 09:21:55 +0200

    lineheight patch

--- config.def.h
+++ config.def.h
@@ -1,28 +1,28 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"DejaVuSansMono-Bold:size=12"
+	"DejaVuSansMono-Bold:size=10"
 	};
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char color0[] = "#12101c";
-static const char color1[] = "#503989";
-static const char color2[] = "#6D52A1";
-static const char color3[] = "#7D66C0";
-static const char color4[] = "#9866A6";
-static const char color5[] = "#D37692";
-static const char color6[] = "#9375C5";
-static const char color7[] = "#ebc1cc";
-static const char color8[] = "#a4878e";
-static const char color9[] = "#503989";
-static const char color10[] = "#6D52A1";
-static const char color11[] = "#7D66C0";
-static const char color12[] = "#9866A6";
-static const char color13[] = "#D37692";
-static const char color14[] = "#9375C5";
-static const char color15[] = "#ebc1cc";
+static const char color0[] = "#0b0f12";
+static const char color1[] = "#835B3E";
+static const char color2[] = "#8E7159";
+static const char color3[] = "#7B837C";
+static const char color4[] = "#A68A73";
+static const char color5[] = "#AB9C93";
+static const char color6[] = "#C7B3A5";
+static const char color7[] = "#e1d9d5";
+static const char color8[] = "#9d9795";
+static const char color9[] = "#835B3E";
+static const char color10[] = "#8E7159";
+static const char color11[] = "#7B837C";
+static const char color12[] = "#A68A73";
+static const char color13[] = "#AB9C93";
+static const char color14[] = "#C7B3A5";
+static const char color15[] = "#e1d9d5";
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { color7, color0 },
@@ -32,6 +32,9 @@ static const char *colors[SchemeLast][2] = {
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
+/* -h option; minimum height of a menu line */
+static unsigned int lineheight = 28;
+static unsigned int min_lineheight = 8;
 
 /*
  * Characters not considered part of a word while deleting words
