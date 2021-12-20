b1ea333f7a2fb46a69ab49f89356bf5c37222dd4
Jodscho <Jonathan2008@gmx.de>
2021-12-20 22:08:03 +0100

    alpha patch

--- config.def.h
+++ config.def.h
@@ -2,6 +2,7 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static const unsigned int alpha = 0xf0;
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"DejaVuSansMono-Bold:size=10"
@@ -25,6 +26,8 @@ static char color13[] = "#968A76";
 static char color14[] = "#C8A378";
 static char color15[] = "#c5c3bc";
 
+static unsigned int border_width = 0;
+
 static char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { color7, color0 },
@@ -32,6 +35,13 @@ static char *colors[SchemeLast][2] = {
 	//[SchemeSel] = { "#000000", "#b5bd68" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
+
+static const unsigned int alphas[SchemeLast][2] = {
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel] = { OPAQUE, alpha },
+	[SchemeOut] = { OPAQUE, alpha },
+};
+
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 /* -h option; minimum height of a menu line */
